2b00519da22f0f326d3df6d6b8636bd265c65108
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-08-16 15:09:23 +0700

    Feature: add getLensName, getFileName, getSystemData, getFieldData, getWaveData functions

--- src/dde/dde_zemax_handler.cpp
+++ src/dde/dde_zemax_handler.cpp
@@ -1,38 +1,41 @@
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <iostream>
 #include <dde.h>
 #include "dde_zemax_handler.h"
 
 namespace ZemaxDDE {
     Logger logger;
+    OpticalSystemData opticalSystem;
 
-    static HWND hwndServer = NULL;
-    static bool GotData = false;
-    static char szBuffer[5000] = {0};
-    static char szItem[256] = {0};
-    static char szTest[256] = {0};
+    HWND hwndServer = NULL;
+    bool GotData = false;
+    char buffer[5000] = {0};
+    char item[256] = {0};
+    char test[256] = {0};
 
-    char* GetString(char* szBuffer, int n, char* szSubString) {
+    char* GetString(char* buffer, int n, char* subString) {
         int i = 0, j = 0, k = 0;
-        char szTest[1000] = {0};
-        szSubString[0] = '\0';
-        while (szBuffer[i] && k <= n) {
-            szTest[j] = szBuffer[i];
-            if (szBuffer[i] == '"') {
+        char temp[1000] = {0};
+        subString[0] = '\0';
+        while (buffer[i] && k <= n) {
+            temp[j] = buffer[i];
+            if (buffer[i] == '"') {
                 i++;
                 j++;
-                szTest[j] = szBuffer[i];
-                while (szBuffer[i] != '"' && szBuffer[i]) {
+                temp[j] = buffer[i];
+                while (buffer[i] != '"' && buffer[i]) {
                     i++;
                     j++;
-                    szTest[j] = szBuffer[i];
+                    temp[j] = buffer[i];
                 }
             }
-            if (szTest[j] == '\n' || szTest[j] == '\r' || szTest[j] == '\0' || szTest[j] == ',' || szTest[j] == ' ') {
-                szTest[j] = '\0';
+            if (temp[j] == '\n' || temp[j] == '\r' || temp[j] == '\0' || temp[j] == ',' || temp[j] == ' ') {
+                temp[j] = '\0';
                 if (k == n) {
-                    strcpy(szSubString, szTest);
-                    return szSubString;
+                    strcpy(subString, temp);
+                    return subString;
                 }
                 k++;
                 j = -1;
@@ -40,26 +43,26 @@ namespace ZemaxDDE {
             i++;
             j++;
         }
-        szTest[j] = szBuffer[i];
-        szTest[j] = '\0';
-        if (k == n) strcpy(szSubString, szTest);
-        return szSubString;
+        temp[j] = buffer[i];
+        temp[j] = '\0';
+        if (k == n) strcpy(subString, temp);
+        return subString;
     }
 
     void WaitForData(HWND hwnd) {
         MSG msg;
         DWORD dwTime = GetTickCount();
         GotData = false;
-        while ((GetTickCount() - dwTime < 10000) && !GotData) {
+        while ((GetTickCount() - dwTime < DDE_TIMEOUT_MS) && !GotData) {
             while (PeekMessage(&msg, hwnd, WM_DDE_FIRST, WM_DDE_LAST, PM_REMOVE)) {
                 DispatchMessage(&msg);
             }
         }
     }
 
-    void PostRequestMessage(const char* szItem, HWND hwndServer, HWND hwnd) {
+    void PostRequestMessage(const char* item, HWND hwndServer, HWND hwnd) {
         wchar_t wItem[256];
-        MultiByteToWideChar(CP_ACP, 0, szItem, -1, wItem, 256);
+        MultiByteToWideChar(CP_ACP, 0, item, -1, wItem, 256);
         ATOM aItem = GlobalAddAtomW(wItem);
         if (!PostMessageW(hwndServer, WM_DDE_REQUEST, (WPARAM)hwnd, PackDDElParam(WM_DDE_REQUEST, CF_TEXT, aItem))) {
             GlobalDeleteAtom(aItem);
@@ -70,8 +73,8 @@ namespace ZemaxDDE {
 
     void initiateDDE(HWND hwnd) {
         hwndServer = NULL;
-        ATOM aApp = GlobalAddAtomW(L"ZEMAX");
-        ATOM aTop = GlobalAddAtomW(L"RayData");
+        ATOM aApp = GlobalAddAtomW(DDE_APP_NAME);
+        ATOM aTop = GlobalAddAtomW(DDE_TOPIC);
         SendMessageW(HWND_BROADCAST, WM_DDE_INITIATE, (WPARAM)hwnd, MAKELONG(aApp, aTop));
         logger.addLog("Sent WM_DDE_INITIATE with app=ZEMAX, topic=RayData");
         GlobalDeleteAtom(aApp);
@@ -88,7 +91,6 @@ namespace ZemaxDDE {
         }
 
         GotData = false;
-
         char request[256];
         snprintf(request, sizeof(request), "GetSurfaceData,%d,2", surfaceNumber);
         logger.addLog(std::string("Sending request: ") + request);
@@ -99,17 +101,140 @@ namespace ZemaxDDE {
             throw std::runtime_error("No response from Zemax");
         }
 
-        logger.addLog(std::string("Raw response: ") + szBuffer);
+        logger.addLog(std::string("Raw response: ") + buffer);
+        char subString[256];
+        GetString(buffer, 0, subString);
+        logger.addLog(std::string("Parsed value: ") + subString);
 
-        char szSubString[256];
-        GetString(szBuffer, 0, szSubString);
-        logger.addLog(std::string("Parsed value: ") + szSubString);
-
-        double curvature = atof(szSubString);
+        double curvature = atof(subString);
         double radius = (curvature != 0.0) ? 1.0 / curvature : 0.0;
         logger.addLog("Calculated radius: " + std::to_string(radius));
 
         return radius;
+    }
+
+    void getLensName(HWND hwnd) {
+        if (!hwndServer) {
+            throw std::runtime_error("DDE connection to Zemax not established");
+        }
+
+        GotData = false;
+        logger.addLog("Sending request: GetName");
+        PostRequestMessage("GetName", hwndServer, hwnd);
+
+        if (!GotData) {
+            logger.addLog("No response from Zemax for GetName");
+            throw std::runtime_error("No response from Zemax for GetName");
+        }
+
+        logger.addLog(std::string("Raw response for GetName: ") + buffer);
+        strncpy(opticalSystem.lensName, buffer, sizeof(opticalSystem.lensName) - 1);
+        opticalSystem.lensName[sizeof(opticalSystem.lensName) - 1] = '\0';
+        if (strlen(opticalSystem.lensName) >= 2) opticalSystem.lensName[strlen(opticalSystem.lensName) - 2] = '\0';
+        logger.addLog(std::string("Parsed Lens Name: ") + opticalSystem.lensName);
+    }
+
+    void getFileName(HWND hwnd) {
+        if (!hwndServer) {
+            throw std::runtime_error("DDE connection to Zemax not established");
+        }
+
+        GotData = false;
+        logger.addLog("Sending request: GetFile");
+        PostRequestMessage("GetFile", hwndServer, hwnd);
+
+        if (!GotData) {
+            logger.addLog("No response from Zemax for GetFile");
+            throw std::runtime_error("No response from Zemax for GetFile");
+        }
+
+        logger.addLog(std::string("Raw response for GetFile: ") + buffer);
+        strncpy(opticalSystem.fileName, buffer, sizeof(opticalSystem.fileName) - 1);
+        opticalSystem.fileName[sizeof(opticalSystem.fileName) - 1] = '\0';
+        if (strlen(opticalSystem.fileName) >= 2) opticalSystem.fileName[strlen(opticalSystem.fileName) - 2] = '\0';
+        logger.addLog(std::string("Parsed File Name: ") + opticalSystem.fileName);
+    }
+
+    void getSystemData(HWND hwnd) {
+        if (!hwndServer) {
+            throw std::runtime_error("DDE connection to Zemax not established");
+        }
+
+        GotData = false;
+        logger.addLog("Sending request: GetSystem");
+        PostRequestMessage("GetSystem", hwndServer, hwnd);
+
+        if (!GotData) {
+            logger.addLog("No response from Zemax for GetSystem");
+            throw std::runtime_error("No response from Zemax for GetSystem");
+        }
+
+        logger.addLog(std::string("Raw response for GetSystem: ") + buffer);
+        char subString[256];
+        opticalSystem.numSurfs = atoi(GetString(buffer, 0, subString));
+        opticalSystem.units = atoi(GetString(buffer, 1, subString));
+        logger.addLog("Parsed numSurfs: " + std::to_string(opticalSystem.numSurfs));
+        logger.addLog("Parsed units: " + std::to_string(opticalSystem.units));
+    }
+
+    void getFieldData(HWND hwnd, int fieldIndex) {
+        if (!hwndServer) {
+            throw std::runtime_error("DDE connection to Zemax not established");
+        }
+
+        GotData = false;
+        char request[256];
+        snprintf(request, sizeof(request), "GetField,%d", fieldIndex);
+        logger.addLog(std::string("Sending request: ") + request);
+        PostRequestMessage(request, hwndServer, hwnd);
+
+        if (!GotData) {
+            logger.addLog("No response from Zemax for GetField");
+            throw std::runtime_error("No response from Zemax for GetField");
+        }
+
+        logger.addLog(std::string("Raw response for GetField: ") + buffer);
+        char subString[256];
+        if (fieldIndex == 0) {
+            opticalSystem.fieldType = atoi(GetString(buffer, 0, subString));
+            opticalSystem.numFields = atoi(GetString(buffer, 1, subString));
+            logger.addLog("Parsed fieldType: " + std::to_string(opticalSystem.fieldType));
+            logger.addLog("Parsed numFields: " + std::to_string(opticalSystem.numFields));
+        } else if (fieldIndex <= 15) {
+            opticalSystem.xField[fieldIndex] = atof(GetString(buffer, 0, subString));
+            opticalSystem.yField[fieldIndex] = atof(GetString(buffer, 1, subString));
+            logger.addLog("Parsed xField[" + std::to_string(fieldIndex) + "]: " + std::to_string(opticalSystem.xField[fieldIndex]));
+            logger.addLog("Parsed yField[" + std::to_string(fieldIndex) + "]: " + std::to_string(opticalSystem.yField[fieldIndex]));
+        }
+    }
+
+    void getWaveData(HWND hwnd, int waveIndex) {
+        if (!hwndServer) {
+            throw std::runtime_error("DDE connection to Zemax not established");
+        }
+
+        GotData = false;
+        char request[256];
+        snprintf(request, sizeof(request), "GetWave,%d", waveIndex);
+        logger.addLog(std::string("Sending request: ") + request);
+        PostRequestMessage(request, hwndServer, hwnd);
+
+        if (!GotData) {
+            logger.addLog("No response from Zemax for GetWave");
+            throw std::runtime_error("No response from Zemax for GetWave");
+        }
+
+        logger.addLog(std::string("Raw response for GetWave: ") + buffer);
+        char subString[256];
+        if (waveIndex == 0) {
+            opticalSystem.primWave = atoi(GetString(buffer, 0, subString));
+            opticalSystem.numWaves = atoi(GetString(buffer, 1, subString));
+            logger.addLog("Parsed primWave: " + std::to_string(opticalSystem.primWave));
+            logger.addLog("Parsed numWaves: " + std::to_string(opticalSystem.numWaves));
+        } else if (waveIndex <= 15) {
+            opticalSystem.waveLen[waveIndex] = atof(GetString(buffer, 0, subString));
+            logger.addLog("Parsed waveLen[" + std::to_string(waveIndex) + "]: " + std::to_string(opticalSystem.waveLen[waveIndex]));
+        }
     }
 
     LRESULT handleDDEMessages(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
@@ -148,14 +273,56 @@ namespace ZemaxDDE {
                 if (pDdeData->cfFormat == CF_TEXT) {
                     wchar_t wItem[256];
                     GlobalGetAtomNameW(aItem, wItem, sizeof(wItem) / sizeof(wchar_t));
-                    WideCharToMultiByte(CP_ACP, 0, wItem, -1, szItem, sizeof(szItem), NULL, NULL);
-                    GetString(szItem, 0, szTest);
+                    WideCharToMultiByte(CP_ACP, 0, wItem, -1, item, sizeof(item), NULL, NULL);
+                    GetString(item, 0, test);
                     GotData = true;
-                    strncpy(szBuffer, (char*)pDdeData->Value, sizeof(szBuffer) - 1);
-                    szBuffer[sizeof(szBuffer) - 1] = '\0';
-                    logger.addLog(std::string("DDE_DATA received, content: ") + szBuffer);
+                    strncpy(buffer, (char*)pDdeData->Value, sizeof(buffer) - 1);
+                    buffer[sizeof(buffer) - 1] = '\0';
+                    logger.addLog(std::string("DDE_DATA received, content: ") + buffer);
 
-                    if (strncmp(szTest, "GetSurfaceData", 14) == 0) {
+                    if (strncmp(test, "GetSurfaceData", 14) == 0) {
+                        DdeAck.fAck = TRUE;
+                    }
+                    if (strcmp(test, "GetName") == 0) {
+                        strncpy(opticalSystem.lensName, (char*)pDdeData->Value, sizeof(opticalSystem.lensName) - 1);
+                        opticalSystem.lensName[sizeof(opticalSystem.lensName) - 1] = '\0';
+                        if (strlen(opticalSystem.lensName) >= 2) opticalSystem.lensName[strlen(opticalSystem.lensName) - 2] = '\0';
+                        DdeAck.fAck = TRUE;
+                    }
+                    if (strcmp(test, "GetFile") == 0) {
+                        strncpy(opticalSystem.fileName, (char*)pDdeData->Value, sizeof(opticalSystem.fileName) - 1);
+                        opticalSystem.fileName[sizeof(opticalSystem.fileName) - 1] = '\0';
+                        if (strlen(opticalSystem.fileName) >= 2) opticalSystem.fileName[strlen(opticalSystem.fileName) - 2] = '\0';
+                        DdeAck.fAck = TRUE;
+                    }
+                    if (strcmp(test, "GetSystem") == 0) {
+                        opticalSystem.numSurfs = atoi(GetString(buffer, 0, test));
+                        opticalSystem.units = atoi(GetString(buffer, 1, test));
+                        DdeAck.fAck = TRUE;
+                    }
+                    if (strcmp(test, "GetField") == 0) {
+                        int fn;
+                        GetString(item, 1, test);
+                        fn = atoi(test);
+                        if (fn == 0) {
+                            opticalSystem.fieldType = atoi(GetString(buffer, 0, test));
+                            opticalSystem.numFields = atoi(GetString(buffer, 1, test));
+                        } else if (fn <= 15) {
+                            opticalSystem.xField[fn] = atof(GetString(buffer, 0, test));
+                            opticalSystem.yField[fn] = atof(GetString(buffer, 1, test));
+                        }
+                        DdeAck.fAck = TRUE;
+                    }
+                    if (strcmp(test, "GetWave") == 0) {
+                        int wn;
+                        GetString(item, 1, test);
+                        wn = atoi(test);
+                        if (wn == 0) {
+                            opticalSystem.primWave = atoi(GetString(buffer, 0, test));
+                            opticalSystem.numWaves = atoi(GetString(buffer, 1, test));
+                        } else if (wn <= 15) {
+                            opticalSystem.waveLen[wn] = atof(GetString(buffer, 0, test));
+                        }
                         DdeAck.fAck = TRUE;
                     }
                 }
