54bfbcb7d31cc1be53f65afac851edbbb8eba4ce
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-08-16 15:08:26 +0700

    Fix: update dde_zemax_handler.h with correct guard macro and extern variables

--- src/dde/dde_zemax_handler.h
+++ src/dde/dde_zemax_handler.h
@@ -1,15 +1,46 @@
-#ifndef DDE_CLIENT_H
-#define DDE_CLIENT_H
+#ifndef DDE_ZEMAX_HANDLER_H
+#define DDE_ZEMAX_HANDLER_H
 
 #include <windows.h>
 #include "logger/logger.h"
 
 namespace ZemaxDDE {
+    struct OpticalSystemData {
+        char lensName[100] = {0};
+        char fileName[100] = {0};
+        int numSurfs = 0;
+        int units = 0;
+        int numFields = 0;
+        int fieldType = 0;
+        double xField[15] = {0};
+        double yField[15] = {0};
+        int numWaves = 0;
+        int primWave = 0;
+        double waveLen[15] = {0};
+    };
+
+    extern OpticalSystemData opticalSystem;
+
     void initiateDDE(HWND hwnd);
-    double getSurfaceRadius(HWND hwnd, int surfaceNumber);
-    LRESULT handleDDEMessages(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
     void terminateDDE();
+    LRESULT handleDDEMessages(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
     extern Logger logger;
+
+    double getSurfaceRadius(HWND hwnd, int surfaceNumber);
+    void getLensName(HWND hwnd);
+    void getFileName(HWND hwnd);
+    void getSystemData(HWND hwnd);
+    void getFieldData(HWND hwnd, int fieldIndex);
+
+    extern HWND hwndServer;
+    extern bool GotData;
+    extern char buffer[5000];
+    extern char item[256];
+    extern char test[256];
+
+    constexpr wchar_t* DDE_APP_NAME = L"ZEMAX";
+    constexpr wchar_t* DDE_TOPIC = L"RayData";
+    constexpr DWORD DDE_TIMEOUT_MS = 10000;
 }
 
-#endif // DDE_CLIENT_H
+#endif // DDE_ZEMAX_HANDLER_H
