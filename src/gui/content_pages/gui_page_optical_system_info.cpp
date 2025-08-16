f247f9bce8dacc24b88855766ed93f8e59919f6e
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-08-16 15:11:12 +0700

    Feature: added output of information about the analyzed optical system

--- src/gui/content_pages/gui_page_optical_system_info.cpp
+++ src/gui/content_pages/gui_page_optical_system_info.cpp
@@ -10,32 +10,59 @@ namespace gui {
         ImGui::TextColored(ImVec4(0.9f, 0.9f, 0.2f, 1.0f), "OPTICAL SYSTEM INFORMATION");
         ImGui::Spacing();
 
-        ImGui::InputInt("Surface Number", &surface_number);
-        if (surface_number < 1) surface_number = 1;
-
-        if (ImGui::Button("Get Radius")) {
-            try {
-                if (dde_initialized) {
-                    if (surface_number <= 0) {
-                        setErrorMsg("Invalid surface number");
-                        return;
-                    }
-
-                    logger.addLog("Calling getSurfaceRadius with hwndDDE: " + std::to_string((uintptr_t)hwndDDE));
-
-                    float radius = ZemaxDDE::getSurfaceRadius(hwndDDE, surface_number);
-                    setRadius(radius);
-                    logger.addLog("Radius retrieved: " + std::to_string(radius));
-                } else {
-                    setErrorMsg("DDE not initialized");
+        try {
+            if (dde_initialized) {
+                ZemaxDDE::getLensName(hwndDDE);
+                ZemaxDDE::getFileName(hwndDDE);
+                ZemaxDDE::getSystemData(hwndDDE);
+                ZemaxDDE::getFieldData(hwndDDE, 0);
+                for (int i = 1; i <= ZemaxDDE::opticalSystem.numFields; i++) {
+                    ZemaxDDE::getFieldData(hwndDDE, i);
                 }
-            } catch (const std::runtime_error& e) {
-                setErrorMsg(e.what());
-                logger.addLog((std::string("Error: ") + e.what()).c_str());
+
+                ImGui::Text("Lens Name: %s", ZemaxDDE::opticalSystem.lensName);
+                ImGui::Text("File Name: %s", ZemaxDDE::opticalSystem.fileName);
+                ImGui::Text("Number of Surfaces: %d", ZemaxDDE::opticalSystem.numSurfs);
+                ImGui::Text("Units: %d", ZemaxDDE::opticalSystem.units); // TODO: Добавить перевод (0=mm, 1=cm, 2=in, 3=m)
+                ImGui::Text("Number of Fields: %d (Type %d)", ZemaxDDE::opticalSystem.numFields, ZemaxDDE::opticalSystem.fieldType);
+                for (int i = 1; i <= ZemaxDDE::opticalSystem.numFields; i++) {
+                    ImGui::Text("Field %d: X=%.4E, Y=%.4E", i, ZemaxDDE::opticalSystem.xField[i], ZemaxDDE::opticalSystem.yField[i]);
+                }
+            } else {
+                setErrorMsg("DDE not initialized");
             }
+        } catch (const std::runtime_error& e) {
+            setErrorMsg(e.what());
+            logger.addLog((std::string("Error: ") + e.what()).c_str());
         }
 
-        ImGui::Text("Radius of Surface %d: %.4f", surface_number, radius);
-        if (errorMsg[0]) ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Error: %s", errorMsg);
+
+        // ImGui::InputInt("Surface Number", &surface_number);
+        // if (surface_number < 1) surface_number = 1;
+
+        // if (ImGui::Button("Get Radius")) {
+        //     try {
+        //         if (dde_initialized) {
+        //             if (surface_number <= 0) {
+        //                 setErrorMsg("Invalid surface number");
+        //                 return;
+        //             }
+
+        //             logger.addLog("Calling getSurfaceRadius with hwndDDE: " + std::to_string((uintptr_t)hwndDDE));
+
+        //             float radius = ZemaxDDE::getSurfaceRadius(hwndDDE, surface_number);
+        //             setRadius(radius);
+        //             logger.addLog("Radius retrieved: " + std::to_string(radius));
+        //         } else {
+        //             setErrorMsg("DDE not initialized");
+        //         }
+        //     } catch (const std::runtime_error& e) {
+        //         setErrorMsg(e.what());
+        //         logger.addLog((std::string("Error: ") + e.what()).c_str());
+        //     }
+        // }
+
+        // ImGui::Text("Radius of Surface %d: %.4f", surface_number, radius);
+        // if (errorMsg[0]) ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Error: %s", errorMsg);
     }
 }
