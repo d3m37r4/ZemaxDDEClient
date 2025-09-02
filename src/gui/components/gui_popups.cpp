3ca34be91c6af04c946871026f5fd7493275c0d1
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-09-02 19:27:37 +0700

    Feature: complete CMake migration with versioning, build modes and clean structure

--- src/gui/components/gui_popups.cpp
+++ src/gui/components/gui_popups.cpp
@@ -1,6 +1,7 @@
 #include "lib/imgui/imgui.h"
 #include "gui/components/gui_popups.h"
 #include "gui/gui.h"
+#include "version.h"
 
 namespace gui {
     void GuiManager::setPopupPosition() {
@@ -29,7 +30,15 @@ namespace gui {
         }
 
         if (ImGui::BeginPopupModal("About", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
-            ImGui::Text("ZemaxDDEClient\nVersion 1.0\n\n(c) 2023 Your Company");
+            ImGui::Text("ZemaxDDE Client");
+            ImGui::Separator();
+            ImGui::Text("Version: %s", APP_FULL_VERSION);
+            ImGui::Text("Built: %s", __DATE__ " " __TIME__);
+            ImGui::Text("Git commit: %s", APP_GIT_COMMIT);
+            ImGui::Text("Compiler: %s", __VERSION__);
+            ImGui::Spacing();
+            ImGui::Text("Author: d3m37r4");
+            ImGui::Text("https://github.com/d3m37r4/ZemaxDDEClient");
             ImGui::Separator();
             if (ImGui::Button("OK", ImVec2(120, 0))) ImGui::CloseCurrentPopup();
             ImGui::EndPopup();
