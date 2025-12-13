6da87a6481c61606b3e2a5346abffe03f1bc876d
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-12-13 20:39:29 +0700

    Feature(gui): enhance About popup with formatted build info and clickable links
    - Replace printf-style text with std::format (C++23) for version, build time, and commit
    - Use ImGui::TextUnformatted for static and formatted strings to improve safety
    - Make Git commit hash and author/repository links clickable via TextLinkOpenURL
    - Construct valid GitHub commit URL using APP_GIT_COMMIT
    - Improve layout with consistent spacing and separators

--- src/gui/components/gui_popups.cpp
+++ src/gui/components/gui_popups.cpp
@@ -1,3 +1,5 @@
+
+
 #include "gui/components/gui_popups.h"
 
 #include "gui/gui.h"
@@ -23,23 +25,49 @@ namespace gui {
     }
 
     void GuiManager::renderAboutPopup() {
-        if (show_about_popup) { 
-            ImGui::OpenPopup("About"); 
-            show_about_popup = false; 
+        if (show_about_popup) {
+            ImGui::OpenPopup("About");
+            show_about_popup = false;
         }
 
         if (ImGui::BeginPopupModal("About", nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
-            ImGui::Text("ZemaxDDE Client");
+            std::string version = std::format("Version: {}", APP_FULL_VERSION);
+            std::string built   = std::format("Built: {} {}", __DATE__, __TIME__);
+            std::string commit  = std::format("Git commit: {}", APP_GIT_COMMIT);
+
+            std::string commitUrl = std::format(
+                "https://github.com/d3m37r4/ZemaxDDEClient/commit/{}",
+                APP_GIT_COMMIT
+            );
+
+            ImGui::TextUnformatted("ZemaxDDEClient");
             ImGui::Separator();
-            ImGui::Text("Version: %s", APP_FULL_VERSION);
-            ImGui::Text("Built: %s", __DATE__ " " __TIME__);
-            ImGui::Text("Git commit: %s", APP_GIT_COMMIT);
-            ImGui::Text("Compiler: %s", __VERSION__);
+
+            ImGui::TextUnformatted("GitHub Repository:");
+            ImGui::SameLine();
+            ImGui::TextLinkOpenURL("github.com/d3m37r4/ZemaxDDEClient",
+                                   "https://github.com/d3m37r4/ZemaxDDEClient");
+
+            ImGui::TextUnformatted("Author:");
+            ImGui::SameLine();
+            ImGui::TextLinkOpenURL("Dmitry Isakov", "https://github.com/d3m37r4");
+
+            ImGui::TextUnformatted(version.c_str());
+            ImGui::TextUnformatted(built.c_str());
+            ImGui::TextUnformatted("Git commit:");
+            ImGui::SameLine();
+            ImGui::TextLinkOpenURL(APP_GIT_COMMIT, commitUrl.c_str());
+
             ImGui::Spacing();
-            ImGui::Text("Author: d3m37r4");
-            ImGui::Text("https://github.com/d3m37r4/ZemaxDDEClient");
             ImGui::Separator();
-            if (ImGui::Button("OK", ImVec2(120, 0))) ImGui::CloseCurrentPopup();
+            ImGui::Spacing();
+
+            float window_width = ImGui::GetWindowSize().x;
+            float button_width = 120.0f;
+            ImGui::SetCursorPosX((window_width - button_width) * 0.5f);
+            if (ImGui::Button("OK", ImVec2(button_width, 0))) {
+                ImGui::CloseCurrentPopup();
+            }
             ImGui::EndPopup();
         }
     }
