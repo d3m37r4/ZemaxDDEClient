3e45c2856658fe93e1dce0ebe418015093bd91d6
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2026-05-22 21:19:49 +0700

    feat: add ZemaxWindowEnumerator for finding zemax.exe windows
    - enumerate all visible zemax.exe windows via Toolhelp32Snapshot + EnumWindows
    
    - return PID, HWND, title for each window
    
    - filter: IsWindowVisible + non-empty title
    
    - minimized windows are NOT filtered (DDE works with them)

---
+++ src/app/zemax_window_enumerator.cpp
@@ -0,0 +1,69 @@
+#include "zemax_window_enumerator.h"
+
+#include <tlhelp32.h>
+
+namespace app {
+
+    struct EnumWindowContext {
+        DWORD targetPid;
+        std::vector<ZemaxWindowInfo>& results;
+    };
+
+    static BOOL CALLBACK enumWindowsProc(HWND hwnd, LPARAM lParam) {
+        auto& ctx = *reinterpret_cast<EnumWindowContext*>(lParam);
+
+        DWORD windowPid = 0;
+        GetWindowThreadProcessId(hwnd, &windowPid);
+
+        if (windowPid != ctx.targetPid) {
+            return TRUE;
+        }
+
+        if (!IsWindowVisible(hwnd)) {
+            return TRUE;
+        }
+
+        wchar_t title[256];
+        int len = GetWindowTextW(hwnd, title, sizeof(title) / sizeof(title[0]));
+        if (len == 0) {
+            return TRUE;
+        }
+
+        ZemaxWindowInfo info;
+        info.pid = ctx.targetPid;
+        info.hwnd = hwnd;
+        info.title.assign(title, static_cast<size_t>(len));
+        ctx.results.push_back(std::move(info));
+
+        return TRUE;
+    }
+
+    static void enumerateWindowsForPid(DWORD pid, std::vector<ZemaxWindowInfo>& results) {
+        EnumWindowContext ctx{pid, results};
+        EnumWindows(enumWindowsProc, reinterpret_cast<LPARAM>(&ctx));
+    }
+
+    std::vector<ZemaxWindowInfo> ZemaxWindowEnumerator::enumerate() {
+        std::vector<ZemaxWindowInfo> results;
+
+        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (snapshot == INVALID_HANDLE_VALUE) {
+            return results;
+        }
+
+        PROCESSENTRY32W pe;
+        pe.dwSize = sizeof(pe);
+
+        if (Process32FirstW(snapshot, &pe)) {
+            do {
+                if (_wcsicmp(pe.szExeFile, L"zemax.exe") == 0) {
+                    enumerateWindowsForPid(pe.th32ProcessID, results);
+                }
+            } while (Process32NextW(snapshot, &pe));
+        }
+
+        CloseHandle(snapshot);
+        return results;
+    }
+
+}
---
+++ src/app/zemax_window_enumerator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <windows.h>
+
+namespace app {
+
+struct ZemaxWindowInfo {
+    DWORD pid;
+    HWND hwnd;
+    std::wstring title;
+};
+
+class ZemaxWindowEnumerator {
+public:
+    static std::vector<ZemaxWindowInfo> enumerate();
+};
+
+}
